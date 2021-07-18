7139c7d8eba1a07dd774fac8c2ca906a42b8696d
DESKTOP-VLAD0I0\icewindful <icewindful@hotmail.com>
2021-07-18 16:46:13 +0800

    test twtich iframe fail

--- src/resources/js/Home.js
+++ src/resources/js/Home.js
@@ -7,6 +7,14 @@ class Home extends Component {
   render() {
     return (
         <div>
+          <iframe
+            src="https://player.twitch.tv/?channel=icewindful&parent=https://www.twitch.tv/icewindful"
+            height="480"
+            width="320"
+            frameborder="1"
+            scrolling="no"
+            allowfullscreen="no">
+          </iframe>
           <h2>更新日誌</h2>
           <h2>2021/7/18 隨行獸</h2>
           <div className="HomeListText"><a href={"https://discord.gg/VbkZwD553m"} target="_blank" rel="noreferrer">Discord回報區</a></div>
